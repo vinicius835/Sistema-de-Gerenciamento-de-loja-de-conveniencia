ba4f74bf354a8183772576a166de1d5bcf6ed3d1
Paulo-r-joao <paulo_r_joao@estudante.sesisenai.org.br>
2024-11-13 17:22:04 -0300

    login.py

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+
 janela = tk.Tk()
 janela.title('Login')
 
@@ -9,28 +9,24 @@ janela.rowconfigure(0,weight=1)
 janela.columnconfigure(0,weight=1)
 
 #Cores Fundo
-# Row 00
 Cor_01 = tk.Label(text='LOGIN',fg='Black', bg='#C0C0C0',width=40,height=3,font=10)
 Cor_01.grid(row=0,column=1,sticky='NSEW',columnspan=4)
-# Row 06
-Cor_02 = tk.Label(fg='Black', bg='#C0C0C0',width=40,height=1)
-Cor_02.grid(row=8,column=1,sticky='NSEW',columnspan=4)
 
-#Espaço em Branco
-# Row 01
+Cor_02 = tk.Label(fg='Black', bg='#C0C0C0',width=40,height=1)
+Cor_02.grid(row=9,column=1,sticky='NSEW',columnspan=4)
+
+#Espaços em Branco
 CorB_01 = tk.Label()
 CorB_01.grid(row=1,column=1)
-# Row 02
+
 CorB_02 = tk.Label()
 CorB_02.grid(row=3,column=1)
 
-# Row 05
-CorB_02 = tk.Label()
-CorB_02.grid(row=5,column=1)
-
-# Row 07
 CorB_03 = tk.Label()
-CorB_03.grid(row=7,column=1)
+CorB_03.grid(row=5,column=1)
+
+CorB_04 = tk.Label()
+CorB_04.grid(row=7,column=1)
 
 #Entrada Usuário
 TextoUsuario = tk.Label(text='Usuário:')
@@ -48,10 +44,8 @@ Senha.grid(row=4,column=2)
 
 #Botão Confirmação
 BotaoLogin = tk.Button(text='Confirmar Login')
-BotaoLogin.grid(row=6,columnspan=3)
+BotaoLogin.grid(row=6,columnspan=4)
+
 
 # Informações Login (Usuário e Senha)
-def InfoLogin():
-    print()
-
 janela.mainloop()
