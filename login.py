33afbfc80b892627fe7b65504ad10cd3cbd39e7c
Paulo-r-joao <paulo_r_joao@estudante.sesisenai.org.br>
2024-11-28 16:11:30 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,4 @@
-# Prototipo - 02
-# - Tela de Login -> SEM FUNÇÂO <-
-# - Somente Tela de Cadastro
+# Prototipo - 03
 import tkinter as tk
 from tkinter import messagebox
 import mysql.connector
@@ -15,27 +13,45 @@ conexao = mysql.connector.connect(
 
 #   Criação e Validação Cliente
 def CriarConta_Info():
-    nome = Usuario2.get()
-    email = Email.get()
-    senha1 = Senha1.get()
-    senha2 = Senha2.get()
+    nome_2 = Usuario2.get()
+    email_2 = Email.get()
+    senha01_2 = Senha1.get()
+    senha02_2 = Senha2.get()
 
-    if not nome or not email or not senha1 or not senha2:
+    if not nome_2 or not email_2 or not senha01_2 or not senha02_2:
         messagebox.showerror("ERRO","Todos os campos devem ser preenchidos!")
         return
-    
-    if senha1 != senha2:
+    if senha01_2 != senha02_2:
         messagebox.showerror("ERRO","As senhas não coincidem")
-    
     try:
         cursor = conexao.cursor()
         sql = "INSERT INTO usuario (Nome, Email, Senha) VALUES (%s, %s, %s)"
-        valores = (nome, email, senha1)
+        valores = (nome_2, email_2, senha01_2)
         cursor.execute(sql, valores)
         conexao.commit()
-        messagebox.showinfo("Sucesso", "Conta criada com sucesso!",)
-        cursor.close()
+        messagebox.showinfo("Sucesso", "Conta criada com sucesso!")
+        cursor.close() 
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
 
+#   Validar Login
+def Validar_Login():
+    email_1 = Usuario1.get()
+    senha_1 = Senha3.get()
+
+    try:
+        # Buscar dados e validar -> (email e senha)
+        cursor = conexao.cursor()
+        sql = "SELECT * FROM usuario WHERE Email = %s AND Senha = %s"
+        valores = (email_1, senha_1)
+        cursor.execute(sql, valores)
+        resultado = cursor.fetchone()
+
+        if resultado:
+            messagebox.showinfo("Sucesso", "Login realizado com sucesso!")
+        else:
+            messagebox.showerror("Erro", "USUÁRIO ou SENHA não encontrado ou incorretos!")
     finally:
         if 'cursor' in locals():
             cursor.close()
@@ -100,14 +116,10 @@ TextoUser1.grid(row=1, column=0, sticky="E")
 
 #   Entrada Senha
 global Senha3
-Senha3 =tk.Entry(janela, show="*", width=30)
+Senha3 =tk.Entry(janela, show="*", width=30,)
 Senha3.grid(row=2, column=1)
 TextoSe3 = tk.Label(janela,text="Senha:")
 TextoSe3.grid(row=2, column=0, sticky="E")
-
-#   Botão 01 - Confirmar Login
-Botao1 = tk.Button(janela, text="Confirmar Login")
-Botao1.grid(row=3, columnspan=2, pady=10)
 
 #   Botão 03 - Janela Criar Conta
 Botao3 = tk.Button(janela,text="Criar Conta", command=CriarConta_Janela)
@@ -117,18 +129,8 @@ Botao3.grid(row=4,columnspan=2)
 Cor = tk.Label(janela,bg="#C0C0C0", height=1)
 Cor.grid(row=5,columnspan=2,sticky="NSEW")
 
-janela.mainloop()
+#   Botão 01 - Confirmar Login
+Botao1 = tk.Button(janela, text="Confirmar Login",command=Validar_Login)
+Botao1.grid(row=3, columnspan=2, pady=10)
 
-#- - - - -Codigos WorkBench SQL- - - - -
-# 
-#   CREATE lojanome;
-#
-#   Use "lojanome";
-#
-#   CREATE TABLE usuario (
-#   id INT AUTO_INCREMENT PRIMARY KEY,
-#   Nome VARCHAR(100) NOT NULL,
-#   Email VARCHAR(100) NOT NULL,
-#   Senha VARCHAR(6) NOT NULL,
-#);
-#
+janela.mainloop()
